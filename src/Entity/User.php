8d835809e975bf5df294536772f1da2c9081050e
Jules Cesar <j.mattei@me.com>
2024-07-15 13:19:51 +0200

    mise ajour de la table user avec les champs manquants

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -39,6 +39,33 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\OneToMany(targetEntity: ParisValeurFonciere::class, mappedBy: 'user')]
     private Collection $parisValeurFoncieres;
 
+    #[ORM\Column(length: 255)]
+    private ?string $telephone = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $no_voie_user = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $voie_user = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $type_voie_user = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $code_postal_user = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $country = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $commune_user = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $code_departement_user = null;
+
+    #[ORM\Column]
+    private ?bool $is_active = null;
+
     public function __construct()
     {
         $this->parisValeurFoncieres = new ArrayCollection();
@@ -145,6 +172,114 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
                 $parisValeurFonciere->setUser(null);
             }
         }
+
+        return $this;
+    }
+
+    public function getTelephone(): ?string
+    {
+        return $this->telephone;
+    }
+
+    public function setTelephone(string $telephone): static
+    {
+        $this->telephone = $telephone;
+
+        return $this;
+    }
+
+    public function getNoVoieUser(): ?string
+    {
+        return $this->no_voie_user;
+    }
+
+    public function setNoVoieUser(string $no_voie_user): static
+    {
+        $this->no_voie_user = $no_voie_user;
+
+        return $this;
+    }
+
+    public function getVoieUser(): ?string
+    {
+        return $this->voie_user;
+    }
+
+    public function setVoieUser(string $voie_user): static
+    {
+        $this->voie_user = $voie_user;
+
+        return $this;
+    }
+
+    public function getTypeVoieUser(): ?string
+    {
+        return $this->type_voie_user;
+    }
+
+    public function setTypeVoieUser(string $type_voie_user): static
+    {
+        $this->type_voie_user = $type_voie_user;
+
+        return $this;
+    }
+
+    public function getCodePostalUser(): ?string
+    {
+        return $this->code_postal_user;
+    }
+
+    public function setCodePostalUser(string $code_postal_user): static
+    {
+        $this->code_postal_user = $code_postal_user;
+
+        return $this;
+    }
+
+    public function getCountry(): ?string
+    {
+        return $this->country;
+    }
+
+    public function setCountry(?string $country): static
+    {
+        $this->country = $country;
+
+        return $this;
+    }
+
+    public function getCommuneUser(): ?string
+    {
+        return $this->commune_user;
+    }
+
+    public function setCommuneUser(?string $commune_user): static
+    {
+        $this->commune_user = $commune_user;
+
+        return $this;
+    }
+
+    public function getCodeDepartementUser(): ?string
+    {
+        return $this->code_departement_user;
+    }
+
+    public function setCodeDepartementUser(?string $code_departement_user): static
+    {
+        $this->code_departement_user = $code_departement_user;
+
+        return $this;
+    }
+
+    public function isActive(): ?bool
+    {
+        return $this->is_active;
+    }
+
+    public function setActive(bool $is_active): static
+    {
+        $this->is_active = $is_active;
 
         return $this;
     }
