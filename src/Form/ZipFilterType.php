421c2b5b0768730b47fa2670c2d1bd997fac4a04
Law <andre75008@gmail.com>
2024-07-18 13:28:40 +0200

    Filters
    filtres de façon séquentielle ok

--- src/Form/ZipFilterType.php
+++ src/Form/ZipFilterType.php
@@ -2,29 +2,21 @@
 
 namespace App\Form;
 
-
-
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\NumberType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 
-class AreaFilterType extends AbstractType
+class ZipFilterType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-          ->add('min_area', NumberType::class, [
-            'required' => false,
-            'label' => 'Min'
-          ])
-          ->add('max_area', NumberType::class, [
-            'required' => false,
-            'label' => 'Max'
-          ]);
-            
-        
+            ->add('code_postal', TextType::class, [
+                'required' => false,
+                'label' => 'code postal'
+            ]);
+        ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
