9d02faaf44e78632cf6fd274a91d4ba963294e9d
Jules Cesar <j.mattei@me.com>
2024-07-24 12:07:36 +0200

    Revert "Modification branch julien"
    This reverts commit 73a7c646db06c610a30f9c11d40b1fb873faa407.

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -5,7 +5,6 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class UserType extends AbstractType
@@ -14,13 +13,7 @@ class UserType extends AbstractType
     {
         $builder
             ->add('email')
-            ->add('roles', ChoiceType::class, [
-                "multiple" => true,
-                "expanded" => true,
-                "choices" => ['ROLE_USER' => 'ROLE_USER',
-                 'ROLE_ADMIN' => 'ROLE_ADMIN',
-                 'ROLE_SUPER_ADMIN' => 'ROLE_SUPER_ADMIN']
-            ])
+            ->add('roles')
             ->add('password')
             ->add('telephone')
             ->add('no_voie_user')
